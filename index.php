de3616f4283903744cdf207019d3676e068898b6
ali-elmuzayan <ali.elmuzayan@gmail.com>
2024-12-18 17:07:59 +0200

    some change in the infrastructure of the Notes folder

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
-
-
+require_once 'autoload.php';
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-
-spl_autoload_register(function (string $class) {
-    $class = str_replace('\\', '/', $class);
-    require "src/$class.php";
-});
 
 use Framework\Dispatcher;
 use Framework\Router;
